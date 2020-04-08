2d4c91d888aa77a0f74234619ab945a13bd40210
PauljadeHS <paul@fischer-fam.de>
2020-04-08 11:29:10 +0200

    20.04.08.11:30:Paul

--- Allgemiene Profilberechnung/Allgemeine Profilberechnungen/Allgemeine Profilberechnungen/Program.cs
+++ Allgemiene Profilberechnung/Allgemeine Profilberechnungen/Allgemeine Profilberechnungen/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Rechteckprofil
+namespace Allgemeine_Profilberechnungen
 {
     class Program
     {
@@ -23,15 +23,15 @@ namespace Rechteckprofil
 
             //Berechnung des Flächeninhaltes 
             double cc = Flächenberechnung(Breite, Höhe);
-            
+
             //Berechnung des Flächenschwerpunktes
             string ff = Flächenschwerpunkt(Breite, Höhe);
 
-            //Berechnung des Flächenträgheitsmomentes IX
-            string gg = Flächenträgheitsmomentix(Breite,Höhe);
+            //Berechnung des Flächenträgheitsmomentes IXX
+            string gg = FlächenträgheitsmomentIXX(Breite, Höhe);
 
-            //Berechnung des Flächenträgheitsmomentes Iy
-            string hh = Flächentregheitsmomentiy(Breite,Höhe);
+            //Berechnung des Flächenträgheitsmomentes IYY
+            string hh = FlächentregheitsmomentIYY(Breite, Höhe);
 
             //Ausgabe 
             Console.WriteLine("Breite = " + Convert.ToString(Breite) + "mm");
@@ -39,8 +39,8 @@ namespace Rechteckprofil
             Console.WriteLine("");
             Console.WriteLine("Flächeninhalt = " + Convert.ToString(cc) + "mm^2"); // Ausgabe Flächeninhalt 
             Console.WriteLine(ff); // Ausgabe Flächenschwerpunkt 
-            Console.WriteLine(gg); //Ausgabe IX
-            Console.WriteLine(hh); // Ausgabe IY 
+            Console.WriteLine(gg); //Ausgabe IXX
+            Console.WriteLine(hh); // Ausgabe IYY 
             Console.WriteLine("Flächentragheitsmoment IXY = 0 mm^4");
 
             Console.ReadKey();
@@ -68,29 +68,30 @@ namespace Rechteckprofil
             return ff;
         }
 
-        static string Flächenträgheitsmomentix(double Breite, double Höhe)
+        static string FlächenträgheitsmomentIXX(double Breite, double Höhe)
         {
             double ix;
             ix = (Breite * Höhe * Höhe * Höhe) / 12;
 
-            string fflk;            
-            fflk = String.Format("Flächenträgheitsmoment IY = {0:00.000}", ix) + "mm^4";
+            string fflk;
+            fflk = String.Format("Flächenträgheitsmoment IXX = {0:00.000}", ix) + "mm^4";
 
             return fflk;
         }
 
-        static string Flächentregheitsmomentiy(double Breite, double Höhe)
+        static string FlächentregheitsmomentIYY(double Breite, double Höhe)
         {
             double iy;
             iy = (Breite * Breite * Breite * Höhe) / 12;
 
             string fflk;
 
-            fflk = String.Format("Flächenträgheitsmoment IY = {0:00.000}", iy) + "mm^4";
+            fflk = String.Format("Flächenträgheitsmoment IYY = {0:00.000}", iy) + "mm^4";
 
             return fflk;
         }
-        
-              
+
+
     }
 }
+    
---
+++ Allgemiene Profilberechnung/Allgemeine Profilberechnungen/Allgemeine Profilberechnungen/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("Allgemeine Profilberechnungen")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Allgemeine Profilberechnungen")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("294820f3-e313-44bc-bb3e-62b9c60daf0d")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
